f460933c6cce2e381eadc339d4f59b1eff08801c
Julliver <giuliacassano1993@gmail.com>
2023-01-23 12:55:57 +0100

    aggiornamento

--- dvd-store/src/card/Card.jsx
+++ dvd-store/src/card/Card.jsx
@@ -1,10 +1,11 @@
 import "./Card.css";
 
-const Card = ({ dvdName, dvdPrice, addButton, removeButton }) => {
+const Card = ({ dvdName, dvdPrice, dvdImage, addButton, removeButton }) => {
   return (
-    <div className="m-4 d-flex flex-column w-20 justify-content-between bg-warning border border-4 border-danger rounded">
+    <div className="m-4 d-flex flex-column w-20 justify-content-between bg-info border border-4 border-danger rounded">
       <div className="p-1">Movie: {dvdName}</div>
       <p className="m-1">Price: {dvdPrice}</p>
+      <img src={dvdImage} alt="" />
       <button className="buttonPlus" onClick={addButton}>
         +
       </button>
